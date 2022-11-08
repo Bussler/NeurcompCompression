01ac9d99246a6c037670f75a9899f7d3e8aa9ced
bussler <maarten.bussler@gmail.com>
2022-11-08 18:26:11 +0100

    Correct Quantization Bug

--- quantization/Net_Dequantizer.py
+++ quantization/Net_Dequantizer.py
@@ -6,7 +6,7 @@ from model.NeurcompModel import Neurcomp
 
 
 # M: Taken from https://github.com/matthewberger/neurcomp
-class NetDecoder:
+class OldNetDecoder:
     def __init__(self):
         pass
 
@@ -91,3 +91,103 @@ class NetDecoder:
                 bdx+=1
 
         return net, self.n_bits
+
+
+# M: Taken from https://github.com/matthewberger/neurcomp
+class NetDecoder:
+    def __init__(self):
+        pass
+    #
+
+    def decode(self,filename):
+        #weight_mats = get_weight_mats(self.net)
+        #bias_vecs = get_bias_vecs(self.net)
+
+        file = open(filename,'rb')
+
+        # header: number of layers
+        self.n_layers = struct.unpack('B', file.read(1))[0]
+        # header: d_in
+        self.d_in = struct.unpack('B', file.read(1))[0]
+        # header: d_out
+        self.d_out = struct.unpack('B', file.read(1))[0]
+        # header: layers
+        self.layers = struct.unpack(''.join(['I' for _ in range(self.n_layers)]), file.read(4*(self.n_layers)))
+        # header: number of bits for clustering
+        self.n_bits = struct.unpack('B', file.read(1))[0]
+        self.n_clusters = int(math.pow(2,self.n_bits))
+
+        net = Neurcomp(input_ch=self.d_in, output_ch=self.d_out, features=self.layers)
+
+        # first layer: matrix and bias
+        w_pos_format = ''.join(['f' for _ in range(self.d_in*self.layers[0])])
+        b_pos_format = ''.join(['f' for _ in range(self.layers[0])])
+        w_pos = th.FloatTensor(struct.unpack(w_pos_format, file.read(4*self.d_in*self.layers[0])))
+        b_pos = th.FloatTensor(struct.unpack(b_pos_format, file.read(4*self.layers[0])))
+
+        all_ws = [w_pos]
+        all_bs = [b_pos]
+
+        # middle layers: cluster, store clusters, then map matrix indices to indices
+        total_n_layers = 2*(self.n_layers-1)
+        res_idx = 0
+        for ldx in range(total_n_layers):
+            # weights
+            n_weights = self.layers[0]*self.layers[res_idx+1]
+            weight_size = (n_weights*self.n_bits)//8
+            if (n_weights*self.n_bits)%8 != 0:
+                weight_size+=1
+            c_format = ''.join(['f' for _ in range(self.n_clusters)])
+            centers = th.FloatTensor(struct.unpack(c_format, file.read(4*self.n_clusters)))
+            inds = file.read(weight_size)
+            bits = ''.join(format(byte, '0'+str(8)+'b') for byte in inds)
+            w_inds = th.LongTensor([int(bits[self.n_bits*i:self.n_bits*i+self.n_bits],2) for i in range(n_weights)])
+
+            if self.n_bits%8 != 0:
+                next_bytes = file.read(4)
+                w_inds[-1] = struct.unpack('I', next_bytes)[0]
+            #
+
+            # bias
+            bias_pointer = 0
+            if ldx % 2 == 0:
+                bias_pointer = res_idx + 1
+
+            b_format = ''.join(['f' for _ in range(self.layers[bias_pointer])])
+            bias = th.FloatTensor(struct.unpack(b_format, file.read(4*self.layers[bias_pointer])))
+
+            w_quant = centers[w_inds]
+            all_ws.append(w_quant)
+            all_bs.append(bias)
+
+            if ldx % 2 != 0:
+                res_idx += 1
+        #
+
+        d = self.layers[-1]
+        # last layer: matrix and bias
+        w_last_format = ''.join(['f' for _ in range(self.d_out*self.layers[0])])
+        b_last_format = ''.join(['f' for _ in range(self.d_out)])
+        w_last = th.FloatTensor(struct.unpack(w_last_format, file.read(4*self.d_out*self.layers[0])))
+        b_last = th.FloatTensor(struct.unpack(b_last_format, file.read(4*self.layers[0])))
+
+        all_ws.append(w_last)
+        all_bs.append(b_last)
+
+        wdx,bdx=0,0
+        for name, parameters in net.named_parameters():
+            if re.match(r'.*.weight', name, re.I):
+                w_shape = parameters.data.shape
+                parameters.data = all_ws[wdx].view(w_shape)
+                wdx+=1
+            #
+            if re.match(r'.*.bias', name, re.I):
+                b_shape = parameters.data.shape
+                parameters.data = all_bs[bdx].view(b_shape)
+                bdx+=1
+            #
+        #
+
+        return net, self.n_bits
+    #
+#
--- quantization/Net_Quantizer.py
+++ quantization/Net_Quantizer.py
@@ -15,7 +15,7 @@ def get_net_weights_biases(net):
     return weights, biases
 
 
-class NetEncoder():
+class OldNetEncoder():
 
     def __init__(self, net: Neurcomp):
         self.net = net
@@ -92,6 +92,103 @@ class NetEncoder():
         bias_format = ''.join(['f' for _ in range(len(biases))])
         file.write(struct.pack(weight_format, *weights))
         file.write(struct.pack(bias_format, *biases))
+
+        file.flush()
+        file.close()
+
+
+def kmeans_quantization(w,q):
+    weight_feat = w#w.view(-1).unsqueeze(1).numpy()
+    kmeans = KMeans(n_clusters=q,n_init=4).fit(weight_feat)
+
+    return kmeans.labels_.tolist(),kmeans.cluster_centers_.reshape(q).tolist()
+
+def ints_to_bits_to_bytes(all_ints,n_bits):
+    f_str = '#0'+str(n_bits+2)+'b'
+    bit_string = ''.join([format(v, f_str)[2:] for v in all_ints])
+    n_bytes = len(bit_string)//8
+    the_leftover = len(bit_string)%8>0
+    if the_leftover:
+        n_bytes+=1
+    the_bytes = bytearray()
+    for b in range(n_bytes):
+        bin_val = bit_string[8*b:] if b==(n_bytes-1) else bit_string[8*b:8*b+8]
+        the_bytes.append(int(bin_val,2))
+    #
+    return the_bytes,the_leftover
+#
+
+# M: Taken from https://github.com/matthewberger/neurcomp
+class NetEncoder():
+
+    def __init__(self, net: Neurcomp):
+        self.net = net
+
+    def encode(self, filename, bit_precision):
+        file = open(filename, 'wb')
+
+        # M: Get basic NW information
+        n_clusters = int(math.pow(2, bit_precision))
+        layer_sizes = self.net.layer_sizes[1:-1] # M: TODO: Find a way to generalize this for all nn
+        n_layers = len(layer_sizes)  # M: haben hier noch in/ out dabei, das ist bei dem orig anders!
+
+        all_weights, all_biases = get_net_weights_biases(self.net)
+
+        # header: number of layers
+        header = file.write(struct.pack('B', 8))
+        # header: d_in
+        header += file.write(struct.pack('B', 3))
+        # header: d_out
+        header += file.write(struct.pack('B', 1))
+        # header: layers
+        header += file.write(struct.pack(''.join(['I' for _ in range(len(layer_sizes))]), *layer_sizes))
+        # header: number of bits for clustering
+        header += file.write(struct.pack('B', bit_precision))
+
+        # first layer: matrix and bias
+        w_pos, b_pos = all_weights[0].view(-1).tolist(), all_biases[0].view(-1).tolist()
+        w_pos_format = ''.join(['f' for _ in range(len(w_pos))])
+        b_pos_format = ''.join(['f' for _ in range(len(b_pos))])
+        first_layer = file.write(struct.pack(w_pos_format, *w_pos))
+        first_layer += file.write(struct.pack(b_pos_format, *b_pos))
+
+        # middle layers: cluster, store clusters, then map matrix indices to indices
+        mid_bias, mid_weight = 0, 0
+        for weight_mat, bias_vec in zip(all_weights[1:-1], all_biases[1:-1]):
+
+            weight_feat = weight_mat.view(-1).unsqueeze(1).numpy()
+            if n_clusters > weight_feat.shape[0]:
+                print('ERROR: Cannot perform K-Means Clustering,'
+                      ' the amount of weights per layer is < than amount of clusters')
+                file.close()
+                return
+
+            labels, centers = kmeans_quantization(weight_feat, n_clusters)
+
+            # weights
+            w = centers
+            w_format = ''.join(['f' for _ in range(len(w))])
+            mid_weight += file.write(struct.pack(w_format, *w))
+            weight_bin, is_leftover = ints_to_bits_to_bytes(labels, bit_precision)
+            mid_weight += file.write(weight_bin)
+
+            # encode non-pow-2 as 16-bit integer
+            if bit_precision % 8 != 0:
+                mid_weight += file.write(struct.pack('I', labels[-1]))
+            #
+
+            # bias
+            b = bias_vec.view(-1).tolist()
+            b_format = ''.join(['f' for _ in range(len(b))])
+            mid_bias += file.write(struct.pack(b_format, *b))
+        #
+
+        # last layer: matrix and bias
+        w_last, b_last = all_weights[-1].view(-1).tolist(), all_biases[-1].view(-1).tolist()
+        w_last_format = ''.join(['f' for _ in range(len(w_last))])
+        b_last_format = ''.join(['f' for _ in range(len(b_last))])
+        last_layer = file.write(struct.pack(w_last_format, *w_last))
+        last_layer += file.write(struct.pack(b_last_format, *b_last))
 
         file.flush()
         file.close()
